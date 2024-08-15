d6cffe711e60ffd5d5368f829bc66df3125059a6
filimonadrian <adrian.filimon@hotmail.com>
2024-08-15 11:22:23 +0300

    Add logic for gracefully shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	config "mem-db/cmd/config"
 	api "mem-db/pkg/api"
 	grpc "mem-db/pkg/api/grpc"
 	httpserver "mem-db/pkg/api/http/server"
+	repo "mem-db/pkg/repository"
+	service "mem-db/pkg/service"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
+
+// func closeApp(ctx context.Context) {
+// 	for {
+// 		select <-
+// 	}
+
+// }
 
 func main() {
 	if len(os.Args) < 2 {
@@ -21,16 +34,42 @@ func main() {
 		panic(fmt.Errorf("Error while trying to read service config: %v", err.Error()))
 	}
 
+	db, wal, err := repo.InitDBSystem(&config.WALOptions)
+	if err != nil {
+		panic(fmt.Errorf("Error while trying to initialize DB system: %v", err.Error()))
+	}
+
+	// check if app is being closed and close resources
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	svc := service.NewService(db, wal)
+
 	var server api.Server
 
 	if config.UseGRPC {
 		server = grpc.NewServer()
 	} else {
-		server = httpserver.NewServer(config.Port)
+		server = httpserver.NewServer(config.Port, svc)
 	}
 
+	// handle gracefully shutdown
+	go wal.KeepSyncing(ctx)
+	go func(ctx context.Context) {
+		select {
+		case <-ctx.Done():
+			shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 3*time.Second)
+			defer shutdownRelease()
+			if err := server.Stop(shutdownCtx); err != nil {
+				// fmt.Printf("Error while stopping the server: %v\n", err.Error())
+				fmt.Println("Error while stopping the server: ", err.Error())
+
+			}
+		}
+	}(ctx)
+
 	if err = server.Start(); err != nil {
-		panic(fmt.Errorf("Error starting server: %v\n", err))
+		panic(fmt.Errorf("Error starting server: %v", err))
 	}
 
 }
