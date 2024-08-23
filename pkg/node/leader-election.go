b75927a338011a285fad15daca2079dd86f35b98
filimonadrian <adrian.filimon@hotmail.com>
2024-08-23 04:53:01 +0300

    Modified leader election algo

--- pkg/node/leader-election.go
+++ pkg/node/leader-election.go
@@ -1,12 +1,14 @@
-package replication
+package node
 
 import (
 	"context"
 	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	leaderelection "k8s.io/client-go/tools/leaderelection"
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
+	config "mem-db/cmd/config"
 	"os"
 	"time"
 )
@@ -25,22 +27,21 @@ func getClusterClientSet() (*kubernetes.Clientset, error) {
 	return clientset, nil
 }
 
-func leaderElection(ctx context.Context) error {
+func (n *Node) leaderElection(ctx context.Context, options *config.NodeOptions) error {
 
-	// logger :=
 	clientset, err := getClusterClientSet()
 	if err != nil {
 		return fmt.Errorf("Error getting clientset: %v\n", err)
 	}
 
-	lock := &resourcelock.ConfigMapLock{
-		ConfigMapMeta: metav1.ObjectMeta{
+	lock := &resourcelock.LeaseLock{
+		LeaseMeta: metav1.ObjectMeta{
 			Name:      "leader-election",
 			Namespace: "default",
 		},
-		Client: clientset.CoreV1(),
+		Client: clientset.CoordinationV1(),
 		LockConfig: resourcelock.ResourceLockConfig{
-			Identity: "node-1",
+			Identity: n.Name,
 		},
 	}
 
@@ -50,29 +51,39 @@ func leaderElection(ctx context.Context) error {
 		RenewDeadline: 10 * time.Second,
 		RetryPeriod:   2 * time.Second,
 		Callbacks: leaderelection.LeaderCallbacks{
-			OnStartedLeading: func(ctx context.Context) {
-				// This node is the leader now
-				runAsMaster()
+			OnStartedLeading: func(_ context.Context) {
+				n.runAsMaster(ctx, options)
 			},
 			OnStoppedLeading: func() {
 				// This node is no longer the leader
-				fmt.Println("Lost leadership, exiting")
+				n.Logger.Warn("Lost leadership, exiting..")
 				os.Exit(0)
 			},
 			OnNewLeader: func(identity string) {
-				// notified when new leader elected
-				if identity == id {
-					// we have the lock
-					runNewLeader()
+				if identity == n.Name {
+					n.Logger.Info("Node elected as new leader: ", identity)
+					// err := n.BroadcastMasterID()
+				} else {
+					n.Logger.Info("New leader elected", identity)
+					n.UpdateMasterID(identity)
 				}
-				fmt.Println("New leader elected", identity)
 			},
 		},
+		ReleaseOnCancel: true,
 	})
 
+	return nil
 }
 
-func runAsMaster() {
+func (n *Node) runAsMaster(ctx context.Context, options *config.NodeOptions) {
+	err := n.Server.Stop(ctx)
+	if err != nil {
+		n.Logger.Error("Server does not stop gracefully: ", err.Error())
+	}
+
+	n.Server = NewMasterHttpServer(ctx, options, n)
+	n.MasterID = ""
+	n.Start(ctx)
 
 }
 
